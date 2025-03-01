1e27d6b1b80e79dba9d66ee1936ca63736a13658
NotMe <112022500+N0TM3-1@users.noreply.github.com>
2025-03-01 18:18:43 +0200

    fixed some stuff

--- pages/handleSubmit.js
+++ pages/handleSubmit.js
@@ -1,14 +1,14 @@
-export default async function handler(req, res) {
-  const SUPABASE_URL = process.env.URL;
-  const SUPABASE_ANON_KEY = process.env.KEY;
+require('dotenv').config({ path: '.env.local' });
 
-  if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-    return res.status(500).json({ error: "Missing Supabase credentials" });
-  }
-}
+console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 
 export const handleSubmit = async (e, email, username) => {
   e.preventDefault();
+
+  const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  console.log(SUPABASE_URL, SUPABASE_ANON_KEY);
 
   const data = {
     email: email,
@@ -18,14 +18,12 @@ export const handleSubmit = async (e, email, username) => {
 
   try {
     const response = await fetch(
-      "${SUPABASE_URL}/rest/v1/emails",
+      `${SUPABASE_URL}/rest/v1/emails`,
       {
         method: "POST",
         headers: {
-          apikey:
-          SUPABASE_ANON_KEY,
-          Authorization:
-            "Bearer ${SUPABASE_ANON_KEY}",
+          apikey: SUPABASE_ANON_KEY,
+          Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
           "Content-Type": "application/json",
           Prefer: "return=minimal",
         },
